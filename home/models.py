06161c0ff23bdf520ca5f2955c4fefc162dbb740
nobelrai <mailtonobelrai@gmail.com>
2023-06-19 18:28:48 +0545

    added add to cart functionality successfully

--- home/models.py
+++ home/models.py
@@ -26,6 +26,8 @@ class Items(models.Model):
     name = models.CharField(max_length=20)
     price = models.IntegerField()
     image = models.ImageField(null=True, blank=True, upload_to="images/")
+    digital = models.BooleanField(default=False)
+
 
     def __str__(self):
         return f"{self.name}"
